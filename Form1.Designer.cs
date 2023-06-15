fd3a86079e640996568c94ab3a8b0936b40d8712
Amrendra Pratap Singh <68188954+Amrendra0003@users.noreply.github.com>
2023-06-15 08:20:39 -0500

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 
+    {
+        private Button selectFolderButton;
+        private void InitializeComponent()
+        {
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(101, 23);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(200, 50);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select Folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 236);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.selectFolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Selection";
+            this.ResumeLayout(false);
+
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderDialog.SelectedPath;
+
+                    // Write the selected folder path to a temporary file
+                    string tempFilePath = Path.Combine(Path.GetTempPath(), "SelectedFolderPath.txt");
+                    File.WriteAllText(tempFilePath, selectedFolderPath);
+
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    DialogResult = DialogResult.Cancel;
+                }
+            }
+        }
+
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string destinationFilePath = Path.Combine(Path.GetTempPath(), "SelectedFolderPath.txt");
+            string sourceFilePath = Path.Combine(Directory.GetCurrentDirectory(), @"file-sample.pdf");
+            string fileContent = File.ReadAllText(destinationFilePath) + @"\file-sample.pdf";
+            try
+            {
+                System.IO.File.Copy(sourceFilePath, fileContent);
+                MessageBox.Show("PDF file downloaded successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("PDF file Already exists");
+            }
+
+        }
+    }
+}
